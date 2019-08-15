cf35c70c33762138dd7374457efcca587d4130fd
congzw <2981810300@qq.com>
2019-08-15 13:43:03 +0800

    add capture service

---
+++ src/PictureServices.Shared/Clients/ICaptureService.cs
@@ -0,0 +1,18 @@
+﻿namespace PictureServices.Clients
+{
+    public interface ICaptureService
+    {
+        //todo create capture
+        CaptureConfig DefaultConfig { get; set; }
+        CaptureResult Capture(CaptureConfig config);
+    }
+
+    public class CaptureResult
+    {
+    }
+
+    public class CaptureConfig
+    {
+
+    }
+}
